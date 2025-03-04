0d0f83dcb08ff8425252afcb91a9f00ac381e42d
ManrajSinghSra <srasinghmanraj@gmail.com>
2025-03-05 01:06:21 +0530

    Con

--- Main.java
+++ Main.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args){
+        List<Integer> list=new ArrayList<>();
+        //after java 7 <Integer> can be omitted
+        // List is an INTERFACE
+        //IT EXTENDS COLLECTION INTERFACE
+
+        list.add(1);
+        list.add(23);
+        list.add(24);
+        for(int x: list){
+            System.out.println(x);
+        }
+        System.out.println(list.contains(3));// for check if something contains or not
+    }
 
 }
